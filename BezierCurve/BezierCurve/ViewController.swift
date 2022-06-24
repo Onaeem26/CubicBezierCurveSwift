d9ac55db19bc7ef6b286c9b93790095c061f8a69
Dino Budimilić <budidino@gmail.com>
2022-06-24 13:20:33 +0200

    Code cleanup
    Just minor tweaks to make the code cleaner and easier to read

--- BezierCurve/BezierCurve/ViewController.swift
+++ BezierCurve/BezierCurve/ViewController.swift
@@ -11,23 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     let bezierPath = UIBezierPath()
-    let data = [CGPoint(x: 60, y: 150), CGPoint(x: 140, y: 300),CGPoint(x: 230, y: 140), CGPoint(x: 310, y: 250), CGPoint(x: 390, y: 100), CGPoint(x: 490, y: 200), CGPoint(x: 580, y: 270),CGPoint(x: 650, y: 30) ]
+    let data = [
+        CGPoint(x: 60, y: 150),
+        CGPoint(x: 140, y: 300),
+        CGPoint(x: 230, y: 140),
+        CGPoint(x: 310, y: 250),
+        CGPoint(x: 390, y: 100),
+        CGPoint(x: 490, y: 200),
+        CGPoint(x: 580, y: 270),
+        CGPoint(x: 650, y: 30)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let config = BezierConfiguration()
-        let controlPoints = config.configureControlPoints(data: data)
-        
+        let controlPoints = config.configureControlPoints(data: data)    
         
         createPoints()
         
-        for i in 0..<data.count {
+        for i in 0 ..< data.count {
             let point = data[i]
             if i == 0 {
                 bezierPath.move(to: point)
-            }else {
+            } else {
                 let segment = controlPoints[i - 1]
-                bezierPath.addCurve(to: point, controlPoint1: segment.firstControlPoint, controlPoint2: segment.secondControlPoint)
+                bezierPath.addCurve(to: point, 
+                                    controlPoint1: segment.firstControlPoint, 
+                                    controlPoint2: segment.secondControlPoint)
             }
         }
         
@@ -45,21 +55,16 @@ class ViewController: UIViewController {
         animation.duration = 2.5
         
         shapeLayer.add(animation, forKey: "drawKeyAnimation")
-        
     }
     
     func createPoints() {
         for point in data {
-          let circleLayer = CAShapeLayer()
-          circleLayer.bounds = CGRect(x: 0, y: 0, width: 10, height: 10)
-          circleLayer.path = UIBezierPath(ovalIn: circleLayer.bounds).cgPath
-          circleLayer.fillColor = UIColor.red.cgColor
-          circleLayer.position = point
-          view.layer.addSublayer(circleLayer)
+            let circleLayer = CAShapeLayer()
+            circleLayer.bounds = CGRect(x: 0, y: 0, width: 10, height: 10)
+            circleLayer.path = UIBezierPath(ovalIn: circleLayer.bounds).cgPath
+            circleLayer.fillColor = UIColor.red.cgColor
+            circleLayer.position = point
+            view.layer.addSublayer(circleLayer)
         }
     }
-    
-
-
 }
-
